b69ff0b652b6b91cb56aa23835beaef2322c2989
DuarteRodrigues <duarte.log.2001@gmail.com>
2024-07-02 17:13:55 +0100

    Comment process in index.js

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Determine what is to be rendered inside the root element
 const Greeting = () => {
     return <h2>My first component</h2>
 };
 
+// Fetch the element inside the document 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+// Render the pre-determined elements inside the root element in the document
 root.render(<Greeting />);

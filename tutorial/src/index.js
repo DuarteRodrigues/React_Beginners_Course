90589067b126c585c56101b73539d53e626364f6
DuarteRodrigues <duarte.log.2001@gmail.com>
2024-07-11 22:47:28 +0100

    JSX rules - className

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 // Determine what is to be rendered inside the root element
 // utilize this one rather than React.createElement, just so you're not calling functions inside functions
 /*const Greeting = () => {
-    return(
+    return (
     <div>
         <div>
             <h3>Hello people</h3>
@@ -20,10 +20,11 @@ import ReactDOM from 'react-dom/client';
 };*/
 
 // This is the same as the above, but with JSX, removing the parent element from inspection
+// In JSX, classes are declared via 'className = '' right after the element reference'
 const Greeting = () => {
-    return(
+    return (
     <React.Fragment>
-        <div>
+        <div className='someValue'> 
             <h3>Hello people</h3>
             <ul>
                 <li>
